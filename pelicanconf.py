8d502e038b4a86a4c2e45136ac81b55501a05c11
Ian Whyman <v00d00@v00d00.net>
2018-12-09 15:11:45 +0000

    Theme, config, test post

---
+++ pelicanconf.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Ian Whyman'
+SITENAME = 'v00d00.net'
+SITEURL = ''
+
+THEME = 'themes/pelican-alchemy/alchemy'
+
+STATIC_PATHS = ['images', 'static']
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# http://docs.getpelican.com/en/stable/plugins.html#how-to-use-plugins
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['pelican-bootstrapify']
+
+BOOTSTRAPIFY = {
+    'table': ['table', 'table-striped', 'table-hover'],
+    'img': ['img-fluid'],
+    'blockquote': ['blockquote'],
+}
+
+SITESUBTITLE = "/dev/random"
+SITEIMAGE = "/images/terminal-solid.svg width=150 height=150"
+
+LINKS = (
+)
+
+ICONS = (
+    ('github', 'https://github.com/v00d00'),
+    ('twitter', 'https://twitter.com/thevoodoo'),
+)
+
+HIDE_AUTHORS = True
